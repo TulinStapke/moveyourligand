330e651731a3e499fcdd6d2033f0c8ffb71d583c
TulinStapke <98519030+TulinStapke@users.noreply.github.com>
2022-03-06 10:35:58 +0100

    Add files via upload

---
+++ 6_Rosetta_scoring_preparation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import os
+
+path_to_ligands=('/path/ligands/')
+# When scoring with the rosetta, ligand chain needs to be named as X and Ligand "ATOM" needs to be replaced with "HETATM"
+
+with os.scandir(path_to_ligands) as lig:
+    for each_file in lig:
+        with open(each_file, 'r') as file:
+            file_data=file.read()    
+            file_data =file_data.replace('L', 'X')
+            file_data=file_data.replace( 'ATOM', 'HETATM')
+        with open(each_file, 'w') as file:
+            file.write(file_data)
